51d9862b4ddfcb47748d4d3f10b888b4712d4289
MariaArnaudova <m.p.arnaudova@gmail.com>
2022-02-04 15:35:01 +0200

    Update

--- MVC_Introduction/BasicWebServer.Demo/Startup.cs
+++ MVC_Introduction/BasicWebServer.Demo/Startup.cs
@@ -50,12 +50,14 @@ Age: <input type='number' name ='Age'/>
                  .MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
                  .MapGet<HomeController>("/Content", c => c.Content())
                  .MapPost<HomeController>("/Content", c => c.DownloadContent())
-                 .MapGet<HomeController>("/Cookies", c => c.Cookies()))
-                 //.MapGet<HomeController>("/Session", c => c.Session()))
+                 .MapGet<HomeController>("/Cookies", c => c.Cookies())
+                 .MapGet<HomeController>("/Session", c => c.Session())
+                 .MapGet<UsersController>("/Login", c => c.Login())
+                 .MapPost<UsersController>("/Login", c => c.LogInUser())
+                 .MapGet<UsersController>("/Logout", c => c.Logout())
+                 .MapGet<UsersController>("/UserProfile", c => c.GetUserData()))
                  .Start();
-            //.MapGet<HomeController>("/Login", new HtmlResponse(Startup.LoginForm))
-            //.MapPost("/Login", new HtmlResponse("", Startup.LoginAction)));
-           
+                              
         }
 
         private static void LoginAction(Request request, Response response)
